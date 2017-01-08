cc55060698be87a8482d6a91d526b55d7861e6f3
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2017-01-08 18:30:24 +0000

    Added api key

--- src/js/src/actions/index.js
+++ src/js/src/actions/index.js
@@ -1,5 +1,10 @@
 import themoviedb from '../themoviedb'
 
+// NOTE: This is bad. In a proper production application,
+// this should be stored in an environment variable and
+// inserted in a variable on the page
+themoviedb.common.api_key = 'd8c0f9e129780554d39905018e863c97'
+
 export function storeResults(results) {
     return {
         type: 'STORE_RESULTS',
